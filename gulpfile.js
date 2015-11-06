bff868b916df55b9fcf08c54dfb05249426570e0
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-06 02:51:19 -0500

    use eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,18 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
-var jshint = require('gulp-jshint');
+var eslint = require('gulp-eslint');
 
 var lint = ['index.js', 'utils.js', 'lib/*.js'];
 
-gulp.task('coverage', function () {
+gulp.task('coverage', function() {
   return gulp.src(lint)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
 });
 
-gulp.task('test', ['coverage'], function () {
+gulp.task('test', ['coverage'], function() {
   return gulp.src('test/*.js')
     .pipe(mocha())
     .pipe(istanbul.writeReports())
@@ -24,10 +23,9 @@ gulp.task('test', ['coverage'], function () {
     }))
 });
 
-gulp.task('lint', function () {
+gulp.task('lint', function() {
   return gulp.src(lint.concat('test/*.js'))
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    .pipe(eslint())
 });
 
 gulp.task('default', ['lint', 'test']);
